417bcf99730787f7b75c8b525b16d3c26c39a40b
AJDavidson <89638345+ajdavidson@users.noreply.github.com>
2022-03-19 08:48:32 -0600

    revamp

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+const e = require('express');
+
+// used to serve static files from public directory
+app.use(express.static('firebase-chat.html'));
+app.use(cors());
+
+const port = process.env.PORT || 3000
+app.listen(port);
+console.log('NOW! Running on port: ' + port);
